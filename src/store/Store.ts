c64fd365e24fa2f59ec911595329aa352a9bbba7
Rajat Tripathi <rivalo3.chat@gmail.com>
2025-04-12 15:51:08 +0530

    Minor changes

---
+++ src/store/Store.ts
@@ -0,0 +1,18 @@
+export abstract class Store{
+    constructor(){
+
+    }
+    initRooom(){
+
+    }
+    getChats(room : string, limit: number , offset: number){
+
+    }
+    addChat(room : string, limit: number , offset: number){
+
+    }
+    upvote(room: string, chatId: string){
+
+    }
+
+}
