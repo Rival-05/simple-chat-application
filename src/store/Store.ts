724d2f3a028b940a9523b8229161dd48a62c785d
Rajat Tripathi <rivalo3.chat@gmail.com>
2025-04-17 22:13:03 +0530

    Init Store.

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,17 +1,26 @@
+export type UserId = string;
+
+export interface Chat {
+    id  :string;
+    userId : UserId;
+    name : string;
+    message : string;
+    upvotes : UserId[];
+}
 export abstract class Store{
     constructor(){
 
     }
-    initRooom(){
+    initRooom(roomId : string){
 
     }
     getChats(room : string, limit: number , offset: number){
 
     }
-    addChat(room : string, limit: number , offset: number){
+    addChat(userId : string , name : string,room : string, message : string){
 
     }
-    upvote(room: string, chatId: string){
+    upvote(userId : string , room: string, chatId: string){
 
     }
 
